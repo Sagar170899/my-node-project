e2e49aa955f03cb805e4503c84f3aedcebe5bde9
Sagar170899 <chavan.g.sagar@gmail.com>
2025-05-12 20:01:06 +0530

    modiefed app.js

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
     res.send('Hello, Jenkins!');
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}/home`);
 });
